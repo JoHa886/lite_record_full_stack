bf2304880b038badf530a823cd8cee7a1e7e5e0b
JoHa886 <shuobuqing@outlook.com>
2021-06-25 15:55:34 +0800

    feat(server): 后端主要框架搭建

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,39 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { resolve } from 'path';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { ConfigModule } from 'nestjs-config';
+import { StatusMonitorModule } from 'nest-status-monitor';
+import statusMonitorConfig from './config/statusMonitor';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { AuthModule } from './modules/auth/auth.module';
+import { HelloModule } from './modules/hello/hello.module';
+import { ExceptionModule } from './modules/exception/exception.module';
+import { RoleGuardModule } from './modules/role-guard/role-guard.module';
+// import { TasksModule } from './tasks/tasks.module';//定时任务
+// import { ScheduleModule } from '@nestjs/schedule';//定时任务
+// import { AudioModule } from './jobs/audio/audio.module'; //redis任务队列相关
+import { AlbumModule } from './modules/album/album.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.load(resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
+    // TODO @nestjs/terminus almost the same
+    StatusMonitorModule.setUp(statusMonitorConfig),
+    AuthModule,
+    HelloModule,
+    ExceptionModule,
+    RoleGuardModule,
+    // ScheduleModule.forRoot(),
+    // TasksModule,
+    // AudioModule,
+    AlbumModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    // 为 hello 路由添加中间件
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'hello', method: RequestMethod.POST })
+      .forRoutes('hello');
+  }
+}
