a771c898f5df98a54d9b94d63da9771f34d6aa72
JoHa886 <shuobuqing@outlook.com>
2021-06-26 14:12:31 +0800

    feat(server): 小修复

--- server/src/common/middleware/logger.middleware.ts
+++ server/src/common/middleware/logger.middleware.ts
@@ -1,9 +1,9 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: () => void) {
+  use(req: Request, res: Response, next: NextFunction) {
     const { method, path } = req;
     console.log(`${method} ${path}`);
     next();
--- server/src/modules/hello/hello.service.ts
+++ server/src/modules/hello/hello.service.ts
@@ -2,8 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class HelloService {
-  fetch(id): string {
-    return `Hello World! ${id}`;
+  fetch(id): any {
+    if (!id) {
+      return 'no';
+    }
+    return {
+      title: 'hahaha',
+      url: 'https://bj29.cn-beijing.data.alicloudccp.com/MoE13mCP%2F51134890%2F6066e080435c9fed9c11482fa0a6bdbd33f231e8%2F6066e080c83caa2dffb4454bbd52a32106a18c86?x-oss-access-key-id=LTAIsE5mAn2F493Q&x-oss-additional-headers=referer&x-oss-expires=1624609951&x-oss-process=image%2Fresize%2Cw_1920%2Fformat%2Cjpeg&x-oss-signature=EL%2FiuFb2WRoWn44BzyG7xO%2FjwtlvsQY%2BQlJjvMpXGtU%3D&x-oss-signature-version=OSS2',
+    };
   }
 
   save(message): string {
